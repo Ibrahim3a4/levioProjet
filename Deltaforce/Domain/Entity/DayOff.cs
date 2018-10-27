23958cbcf7668b0c4a549546aa030243ad46c85e
Ibrahim3a4 <35639176+Ibrahim3a4@users.noreply.github.com>
2018-10-27 17:55:05 +0200

    ProjetLevio
    projet

---
+++ Deltaforce/Domain/Entity/DayOff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class DayOff
+    {
+        public DateTime DayoffId { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public String Reason { get; set; }
+
+        public virtual ICollection<Resource> Resources { get; set; }
+    }
+}
---
+++ Deltaforce/Domain/Entity/Resource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public  class Resource : User
+    {
+        public String Seniority { get; set; }
+        public String BusinessProfile  { get; set; }
+        public int Rating { get; set; }
+        public String CV { get; set; }
+
+        public ContractType Contract { get; set; }
+        public  AvailabilityState Availability { get; set; }
+        public String Photo { get; set; }
+
+        public DateTime HiringDate { get; set; }
+        public float Salary { get; set; }
+
+        public virtual ICollection<Skill> Skills { get; set; }
+
+        public virtual ICollection<DayOff> Dayoffs { get; set; }
+
+        public virtual ICollection<Holiday> Holidays  { get; set; }
+
+    }
+}
