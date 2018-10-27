21d93e7ebaeafebb7b047778679ea744ef0c698c
Sabrine Gmati <sabrine.gmati@esprit.tn>
2018-10-27 22:20:05 +0100

    “Deltaforce/Domain/Entity/InterMandate.cs”

---
+++ Deltaforce/Domain/Entity/InterMandate.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public  class InterMandate 
+    {
+        public int InterMandateId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public virtual ICollection<Resource>  Resources { get; set; }
+
+    }
+}
